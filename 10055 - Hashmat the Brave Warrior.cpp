e74183b3cd3dff9897fbce53427e66f1a764971f
Md. Minhajul Islam <66138525+wrench786@users.noreply.github.com>
2020-11-04 21:57:08 +0600

    Added 10055 - Hashmat the Brave Warrior.cpp
    Problem Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=94&page=show_problem&problem=996

---
+++ 10055 - Hashmat the Brave Warrior.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define wrench786 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define i64 long long
+#define PI (acos(-1.0))
+#define LIMIT 1000000007
+using namespace std;
+
+int main() {
+    wrench786
+
+    i64 int a,b;
+    while(cin>>a>>b){
+        cout<<abs(b-a)<<endl;
+    }
+}
