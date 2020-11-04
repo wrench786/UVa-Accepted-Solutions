be847b18ea5b1049780f895af1145ef136872588
Md. Minhajul Islam <66138525+wrench786@users.noreply.github.com>
2020-11-04 22:12:10 +0600

    Added 10071 - Back to High School Physics.cpp
    Problem Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=94&page=show_problem&problem=1012

---
+++ 10071 - Back to High School Physics.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define wrench786 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define i64 long long
+#define PI (acos(-1.0))
+#define LIMIT 1000000007
+using namespace std;
+
+int main() {
+    wrench786
+
+    int v,t;
+    while(cin>>v>>t){
+        cout<<2*v*t<<endl;
+    }
+}
