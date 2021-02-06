767c154b61cc7155489dc74c1242e736bbe3ec0d
Md. Minhajul Islam <66138525+wrench786@users.noreply.github.com>
2021-02-06 21:07:07 +0600

    Added 11150 - Cola.cpp
    Problem Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=2091

---
+++ 11150 - Cola.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define wrench786 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define PI (acos(-1.0))
+#define LIMIT 1000000000
+using namespace std;
+
+int main(){
+    wrench786
+
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+
+    int n;
+    while(cin>>n){
+        int ans=n;
+        while(n){
+            ans+=n/3;
+            n= (n%3)+n/3;
+            if(n<3){
+                if(n==2) ans++;
+                break;
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+#ifndef ONLINE_JUDGE
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+
+}
