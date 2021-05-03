92c5fee5bb10f4da4e4909578edc9205be9d4fee
Md. Minhajul Islam <66138525+wrench786@users.noreply.github.com>
2021-05-04 00:54:25 +0600

    Added 1124 - Celebrity jeopardy.cpp
    Problem Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3565

---
+++ 1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define wrench786 ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define PI (acos(-1.0))
+#define LIMIT 1000000
+#define pb push_back
+#define dot(x) fixed<<setprecision(x)
+using namespace std;
+
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+    clock_t tStart = clock();
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+
+    string str; while(getline(cin,str)) cout<<str<<endl;
+
+#ifndef ONLINE_JUDGE
+    fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+#endif
+
+}
